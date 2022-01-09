666c54d37e45ef9ac07f37d08fe0fd6e93f1e120
rishabhranawat <rishabhranawat12345@gmail.com>
2022-01-09 10:23:37 -0800

    updates

--- background.js
+++ background.js
@@ -1,10 +1,3 @@
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
-
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.url) {
         if (changeInfo.url.includes("twitter")) {
